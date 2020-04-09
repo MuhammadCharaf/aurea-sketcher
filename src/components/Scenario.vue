3b76bb8f8548b804509428fa95d09e67a54a1441
Muhammad Charaf <muhammad.charaf@gmail.com>
2020-04-09 08:33:19 +0200

    --wip-- [skip ci]

--- src/components/Scenario.vue
+++ src/components/Scenario.vue
@@ -4,7 +4,7 @@
             <div class="menu">
                 <div class="field is-horizontal menu-label">
                     <div class="field-label is-normal">
-                        <label class="label">Scenario</label>
+                        <label class="label">Scenario:</label>
                     </div>
                     <div class="field-body">
                         <div class="field">
@@ -13,6 +13,7 @@
                                     class="input is-small is-rounded"
                                     type="text"
                                     placeholder="Scenario Title"
+                                    v-model="scenario.title"
                                 />
                             </p>
                         </div>
